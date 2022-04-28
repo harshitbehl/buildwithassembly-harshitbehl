821a3f2c9b1fd93cfb5b3ca89051fa24dd2f7b1e
Harshit Behl <harshit.behl.96@gmail.com>
2022-04-28 00:26:22 -0400

    feat(home): implement react router & home page

---
+++ client/src/components/showcase/Showcase.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Showcase.scss";
+
+function Showcase() {
+  return <section className="showcase">Showcase</section>;
+}
+
+export default Showcase;
---
+++ client/src/components/task/Task.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Task.scss";
+
+function Task() {
+  return <section className="task">Task</section>;
+}
+
+export default Task;
