2353a955f79e5721db4dc54e359c8b9a0f3106da
Harshit Behl <harshit.behl.96@gmail.com>
2022-05-01 15:30:38 -0400

    feat(task): add task form component & implement conditional rendering for results

--- client/src/components/task/Task.jsx
+++ client/src/components/task/Task.jsx
@@ -1,15 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Task.scss";
 
 import TaskForm from "./TaskForm";
 import { FaGithub } from "react-icons/fa";
+import Avatar from "@mui/material/Avatar";
+import notFoundIllustration from "../../assets/svg/notFoundIllustration.svg";
+import taskInitialIllustration from "../../assets/svg/taskInitialIllustration.svg";
 
 // Redux Imports
 import { useSelector } from "react-redux";
 
 function Task() {
+  const [status, setStatus] = useState("Initial");
+
   // Redux Functions
   const results = useSelector((state) => state.search.results);
+  console.log(results);
 
   return (
     <section className="task" id="task-section">
@@ -23,9 +29,42 @@ function Task() {
             <h3>
               <FaGithub /> GitHub Search
             </h3>
-            <TaskForm />
+            <TaskForm status={status} setStatus={setStatus} />
           </div>
-          <div className="task__results-container"></div>
+          {status === "Initial" && (
+            <div className="task__initial">
+              <img
+                src={taskInitialIllustration}
+                alt="Task Initial Illustration"
+              />
+            </div>
+          )}
+          {status === "Success" && (
+            <div className="task__results-container">
+              {results.map((result) => (
+                <div
+                  className={`task__result-card ${
+                    result.type === "User" ? "user" : "organization"
+                  }`}
+                  key={result.id}
+                >
+                  <Avatar
+                    src={result.avatar_url}
+                    sx={{ height: 70, width: 70 }}
+                  />
+                  <p>{result.login}</p>
+                  <a href={result.html_url} target="_blank">
+                    GitHub Profile
+                  </a>
+                </div>
+              ))}
+            </div>
+          )}
+          {status === "No Results Found" && (
+            <div className="task__not-found">
+              <img src={notFoundIllustration} alt="Not Found Illustration" />
+            </div>
+          )}
         </div>
       </div>
     </section>
--- client/src/components/task/TaskForm.jsx
+++ client/src/components/task/TaskForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./TaskForm.scss";
 
 import { Formik, Form, Field, ErrorMessage } from "formik";
@@ -8,27 +8,41 @@ import axios from "axios";
 import { useDispatch } from "react-redux";
 import { setResults } from "../../features/search/searchSlice";
 
-function TaskForm() {
+function TaskForm({ status, setStatus }) {
   // Redux Functions
   const dispatch = useDispatch();
 
+  // Form Submit Handler Function
   const handleSubmit = async (values, { resetForm }) => {
     try {
+      setStatus("In Progress...");
       if (values.searchCondition === "users") {
         const res = await axios.get(
           `https://api.github.com/search/users?q=${values.search}`
         );
+
         dispatch(setResults(res.data.items));
-        // console.log(res.data.items);
+        setStatus("Success");
+      } else if (values.searchCondition === "organizations") {
+        const res = await axios.get(
+          `https://api.github.com/search/users?q=${values.search}+type:org`
+        );
+
+        dispatch(setResults(res.data.items));
+        setStatus("Success");
       }
+
+      // Clear Fields After Form Submission
+      resetForm({ values: "" });
     } catch (error) {
       console.log(error);
+      setStatus("No Results Found");
     }
   };
 
   return (
     <Formik
-      initialValues={{ searchCondition: "", search: "" }}
+      initialValues={{ searchCondition: "users", search: "" }}
       onSubmit={handleSubmit}
     >
       {({ values }) => (
@@ -40,9 +54,20 @@ function TaskForm() {
             <label htmlFor="searchCondition">Organizations</label>
           </div>
           <div className="task-form__search-container">
-            <Field type="search" name="search" placeholder="Search GitHub" />
+            <Field
+              type="search"
+              name="search"
+              placeholder={
+                values.searchCondition === "users"
+                  ? "Search Users"
+                  : "Search Organizations"
+              }
+            />
             <button type="submit">Search</button>
           </div>
+          <p className="task-form__status">
+            Status: <span>{status}</span>
+          </p>
         </Form>
       )}
     </Formik>
